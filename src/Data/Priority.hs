259bc06376ff225efc6656399087b3dbb05be364
Anselm Schüler <mail@anselmschueler.com>
2022-04-20 13:52:24 +0200

    Fixed comparison of Higher & Lower

--- src/Data/Priority.hs
+++ src/Data/Priority.hs
@@ -18,11 +18,11 @@ higher a = Higher (Base a)
 instance Ord b => Ord (Priority b) where
     compare   (Lower p)    (Lower q)  = compare p q
     compare x@(Lower p)    (Base b)   = compareBase x b
-    compare   (Lower p)  y@(Higher q) = ifEQ LT $ compare p y
+    compare   (Lower p)    (Higher q) = ifEQ LT $ compare p q
     compare   (Base a)   y@(Lower q)  = flipOrdering $ compareBase y a
     compare   (Base a)     (Base b)   = compare a b
     compare   (Base a)   y@(Higher q) = flipOrdering $ compareBase y a
-    compare   (Higher p) y@(Lower q)  = ifEQ GT $ compare p y
+    compare   (Higher p)   (Lower q)  = ifEQ GT $ compare p q
     compare x@(Higher p)   (Base b)   = compareBase x b
     compare   (Higher p)   (Higher q) = compare p q
 
